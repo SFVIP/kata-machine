910648a614988ff257d29f1849d273d3af94cdaa
Simon Foo <kueypingfoo@asl.com.hk>
2026-05-07 15:47:37 +0800

    Improved QuickSort's partition function

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -28,19 +28,14 @@ function qs(arr: number[], lo: number, hi: number): void {
 }
 
 function partition(arr: number[], lo: number, hi: number): number {
-    const pivot = arr[hi];
-    let idx = lo - 1;
+    let idx = lo;
     for (let i = lo; i < hi; ++i) {
-        if (arr[i] <= pivot) {
+        if (arr[i] <= arr[hi]) {
+            [arr[i], arr[idx]] = [arr[idx], arr[i]]
             idx++;
-            const temp = arr[i];
-            arr[i] = arr[idx];
-            arr[idx] = temp;
         }
     }
-    idx++;
-    arr[hi] = arr[idx];
-    arr[idx] = pivot;
+    [arr[hi], arr[idx]] = [arr[idx], arr[hi]];
     return idx;
 }
 
